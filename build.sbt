08420bb69f3ada5f17323cbbcd216beabd8070e4
prasad <dunga.prasad@datafactz.com>
2018-01-08 16:13:30 +0530

    Initial commit

---
+++ build.sbt
@@ -0,0 +1,9 @@
+name := "Hello-Akka"
+
+version := "1.0"
+
+scalaVersion := "2.12.4"
+
+libraryDependencies ++= Seq(
+  "com.typesafe.akka" %% "akka-actor" % "2.5.8"
+)
---
+++ src/main/scala/com/dfz/akka/HelloAkkaScala.scala
@@ -0,0 +1,26 @@
+package com.dfz.akka
+
+import akka.actor.{Actor, ActorSystem, Props}
+
+/**
+  * Created by prasad on 1/8/2018.
+  */
+
+//define actor message
+case class WhoToGreet(who: String)
+
+//define greeter actor
+class Greeter extends Actor{
+  def receive = {
+    case WhoToGreet(who) => println(s"Hello $who")
+  }
+}
+
+object HelloAkkaScala extends App{
+  //create the 'hello akka' actor system
+  val system = ActorSystem("Hello-Akka")
+  //craete the 'greeter actor'
+  val greeter = system.actorOf(Props[Greeter],"greeter")
+//send whotogreet messege to actor
+  greeter ! WhoToGreet("Akka")
+}
