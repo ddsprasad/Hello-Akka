f951ec14588f9ebb091cefa282fcf0f5b75716e2
prasad <dunga.prasad@datafactz.com>
2018-01-08 17:34:30 +0530

    ActorCreation

---
+++ src/main/scala/com/dfz/akka/ActorCreation.scala
@@ -0,0 +1,69 @@
+package com.dfz.akka
+
+import akka.actor.{ActorSystem, Props, Actor}
+import com.dfz.akka.MusicController.{Stop, Play}
+import com.dfz.akka.MusicPlayer.{StratMusic, StopMusic}
+
+/**
+  * Created by prasad on 1/8/2018.
+  */
+//Music controller msges
+object MusicController {
+  sealed trait ControllerMsg
+  case object Play extends ControllerMsg
+  case object Stop extends ControllerMsg
+
+  def props = Props[MusicController]
+}
+
+//Music controller
+
+class MusicController extends Actor {
+  import MusicController._
+
+  def receive = {
+    case Play =>
+      println("Music Started .....")
+    case Stop =>
+      println("Music Stopped......")
+    case _=>
+      println("UnKnown")
+  }
+
+}
+
+//Music player masges
+object MusicPlayer {
+  sealed trait PlayMsg
+  case object StopMusic extends PlayMsg
+  case object StratMusic extends PlayMsg
+}
+
+//Music player
+class MusicPlayer extends Actor {
+  import MusicPlayer._
+
+  def receive = {
+    case StopMusic =>
+      println("I dont want to play Music")
+    case StratMusic =>
+      val controller = context.actorOf(MusicController.props,"Contoller")
+      controller ! Play
+  }
+}
+
+object Creation extends App{
+  val system = ActorSystem("Creation")
+  val  player = system.actorOf(Props[MusicPlayer],"Player")
+
+  player ! StratMusic
+//  player ! StopMusic
+
+  system.terminate()
+}
+
+
+
+
+
+
